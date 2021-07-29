ff2cce831f7ed1b4e74a25a41083bda0d54fa7ab
Berker <berkergng457@gmail.com>
2021-07-29 15:02:22 +0300

    editor lib added

---
+++ js/ide.js
@@ -0,0 +1,43 @@
+let editor;
+
+function initEditor() {
+	editor = ace.edit("editor");
+	editor.setTheme("ace/theme/monokai");
+	editor.session.setMode("ace/mode/c_cpp");
+}
+
+function run() {
+	$.ajax({
+		url: "/app/compiler.php",
+		method: "POST",
+		data: {
+			language: document.getElementById("language").value,
+			code: editor.getSession().getValue()
+		},
+
+		success: function(response) {
+			document.getElementById("output").innerText = response;
+		}
+	})
+}
+
+function changeLanguage() {
+	const language = document.getElementById("language").value;
+
+	switch (language) {
+		case "cpp":
+			editor.session.setMode("ace/mode/c_cpp");
+			break;
+		case "python":
+			editor.session.setMode("ace/mode/python");
+			break;
+		case "node":
+			editor.session.setMode("ace/mode/javascript");
+			break;
+		case "php":
+			editor.session.setMode("ace/mode/php");
+			break;
+	}
+}
+
+initEditor();
